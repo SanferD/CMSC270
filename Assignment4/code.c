95c956baee516f94922d184464721adee7d647b6
Sanfer D'souza <sanferdsouza@gmail.com>
2015-04-28 15:05:06 -0500

    A4 with only fstream.h

---
+++ Assignment4/code.c
@@ -0,0 +1,28 @@
+if(n==1)      
+
+
+		{		n = 2;n=3;n= 	 3,n=4;		   i=2;}
+	else 
+{a = "words";
+b= "words also";if (a = b)
+{
+	f(a^2);
+	b = 2*2;				if(a ==b || b == c)
+
+
+	{
+					c += 2;
+			else
+
+	{
+
+		cout << f(1,2) << 3;
+		while(x)
+{
+			cout << "in while" << endl;
+				}
+							}
+	}
+}
+}
+
--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -1,5 +1,60 @@
+#include <fstream>
+
+#define SKIP(ch) (ch==' ' || ch=='\n' || ch=='\t')
+#define DELIM(ch) (ch=='{'||ch=='}'||ch==' '||ch=='\n'||ch==';')
+
+void format(std::ifstream&, std::ofstream&, int ind = 0);
+char *indent(int);
+
 int main()
 {
+    std::ifstream src("C:/Programs/CMSC270/Assignment4/code.c");
+    std::ofstream dest("C:/Programs/CMSC270/Assignment4/formatted.c");
+
+    format(src,dest);
 
     return 0;
+}
+
+void format(std::ifstream& src, std::ofstream& dest, int ind)
+{
+    using std::string;
+
+    while(src.peek() != EOF)
+    {
+        char ch = src.get();
+
+        if(DELIM(ch)) for(;SKIP(src.peek());src.get());
+        switch(ch)
+        {
+        case ' ':
+            if(src.peek() != '{') dest << ch;
+            break;
+        case '{':
+            dest << ' ' << ch << '\n' << indent(ind+1);
+            format(src,dest,ind+1);
+            dest << indent(ind) << '}' << '\n';
+            break;
+        case '}':
+            return;
+        case ';':
+            dest << ch << "\n";
+            if(src.peek() != '}') dest << indent(ind);
+            break;
+        case '\n':
+            if(src.peek() != '{') dest << '\n';
+            break;
+        default:
+            dest << ch;
+            break;
+        }
+    }
+    return;
+}
+
+char *indent(int num)
+{
+    char *tabs = (char*) calloc(num+1, sizeof(char));
+    for(int i=0;i!=num;tabs[i++]='\t');
+    return tabs;
 }
