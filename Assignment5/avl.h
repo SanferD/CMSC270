aed7f8ddd290438b57f621ae5b860359f2b8e907
Sanfer D'souza <sanferdsouza@gmail.com>
2015-05-11 22:05:24 -0500

    A5 Done.

--- Assignment5/avl.h
+++ Assignment5/avl.h
@@ -1,8 +1,8 @@
 #ifndef AVL
 #define AVL
 
-#include<iostream>
-using namespace std;
+#include <iostream>
+
 /* AVLNode */
 template<class T>
 class AVLNode
@@ -41,33 +41,133 @@ public:
         snapShot(out,root);
         out << "]" << std::endl;
     }
+
+    bool isValid() // wrapper.
+    {
+        return isValid(root);
+    }
+
+    bool isBalanced() // wrapper.
+    {
+        return isBalanced(root);
+    }
+
 private:
     AVLNode<T>* root;
 
-//    void rightRotation(AVLNode<T>* &node) {
-//        AVLNode<T>* left = node->left;
+    /* For nodes whose left child has a right child.
+     */
+    void leftRightSwap(AVLNode<T>* &node)
+    {
+        AVLNode<T> *left = node->left;
 
-//        if(left)
-//        {
-//            node->left = left->right;
-//            left->right = node;
-//            node = left;
-//        }
-//        else
-//        {
-//            node->balance--;
-//            node->right->left = new AVLNode<T>(node->key);
-//            node = node->right;
-//        }
-//    }
+        AVLNode<T> *pivot = left->right;
+        AVLNode<T> *Q = pivot->left, *R = pivot->right;
+
+        int nb = node->balance, lb = left->balance, pb = pivot->balance;
+
+        pivot->left = left;
+        pivot->right = node;
+        left->right = Q;
+        node->left = R;
+        node = pivot;
+
+        int balL = node->left->balance = (pb>=0) ? lb-pb-1 : lb-1;
+
+        if(lb>=0 && pb>=0)
+        {
+            int balR = node->right->balance = nb+2;
+            if(balL<=0 && balR<=0) node->balance = lb-1;
+            else if(balL<=0 && balR>0) node->balance = nb+lb+1;
+            else if(balL>0 && balR<=0) node->balance = pb;
+            else node->balance = nb+pb+2;
+        }
+        else if(lb<0 && pb>=0)
+        {
+            int balR = node->right->balance = nb-lb+2;
+            if(balL<=0 && balR<=0) node->balance = lb-1;
+            else if (balL<=0 && balR>0) node->balance = nb+1;
+            else if(balL>0 && balR<=0) node->balance = pb;
+            else node->balance = nb-lb+pb+2;
+        }
+        else if(lb>=0 && pb<0)
+        {
+            int balR = node->right->balance = nb-pb+2;
+            if(balL<=0 && balR<=0) node->balance = lb+pb-1;
+            else if(balL<=0 && balR>0) node->balance = nb+lb+1;
+            else if(balL>0 && balR<=0) node->balance = pb;
+            else node->balance = nb+2;
+        }
+        else // lb<0 && pb<0
+        {
+            int balR = node->right->balance = nb-lb-pb+2;
+            if(balL<=0 && balR<=0) node->balance = lb+pb-1;
+            else if (balL<=0 && balR>0) node->balance = nb+1;
+            else if(balL>0 && balR<=0) node->balance = pb;
+            else node->balance = nb-lb+2;
+        }
+
+    }
+
+    /* For nodes whose right child has a left child.
+     */
+    void rightLeftSwap(AVLNode<T>* &node)
+    {
+        AVLNode<T>* right = node->right;
+
+        AVLNode<T> *pivot = right->left;
+        AVLNode<T> *Q = pivot->left, *R = pivot->right;
+
+        int nb = node->balance, rb = right->balance, pb = pivot->balance;
+
+        pivot->right = right;
+        pivot->left = node;
+        right->left = R;
+        node->right = Q;
+        node = pivot;
+
+        int balR = node->right->balance = (pb<=0) ? rb-pb+1 : rb+1;
+
+        if(rb<=0 && pb<=0)
+        {
+            int balL = node->left->balance = nb-2;
+            if(balL>=0 && balR>=0) node->balance = rb+1;
+            else if(balL<0 && balR>=0) node->balance = nb+rb-1;
+            else if(balL>=0 && balR<0) node->balance = pb;
+            else node->balance = nb+pb-2;
+        }
+        else if(rb>0 && pb<=0)
+        {
+            int balL = node->left->balance = nb+rb-2;
+            if(balL>=0 && balR>=0) node->balance = rb+1;
+            else if(balL<0 && balR>=0) node->balance = nb-1;
+            else if(balL>=0 && balR<0) node->balance = pb;
+            else node->balance = nb-rb+pb-2;
+        }
+        else if(rb<=0 && pb>0)
+        {
+            int balL = node->left->balance = nb-pb-2;
+            if(balL>=0 && balR>=0) node->balance = rb+pb+1;
+            else if(balL<0 && balR>=0) node->balance = nb+rb-1;
+            else if(balL>=0 && balR<0) node->balance = pb;
+            else node->balance = nb-2;
+        }
+        else // rb>0 && pb>0
+        {
+            int balL = node->left->balance = nb-rb-pb-2;
+            if(balL>=0 && balR>=0) node->balance = rb+pb+1;
+            else if (balL<0 && balR>=0) node->balance = nb-1;
+            else if(balL>=0 && balR<0) node->balance = pb;
+            else node->balance = nb-rb-2;
+        }
+
+    }
 
     void rightRotation(AVLNode<T>* &node)
     {
-        cout << "rightRot: " << node->key << endl;
-        AVLNode<T> *left = node->left;
-
         if(node->left->left)
         {
+            AVLNode<T> *left = node->left;
 
             int nb = node->balance, lb = left->balance;
 
@@ -78,67 +178,18 @@ private:
             int bal = node->right->balance = (lb<=0) ? nb-lb+1 : nb+1;
             node->balance = (bal<=0) ? lb+1 : lb+bal+1;
         }
-        else
-        {
-            AVLNode<T> *pivot = left->right;
-            AVLNode<T> *Q = pivot->left, *R = pivot->right;
+        else if(node->left->right) leftRightSwap(node);
 
-            int nb = node->balance, lb = left->balance, pb = pivot->balance;
-
-            pivot->left = left;
-            pivot->right = node;
-            left->right = Q;
-            node->left = R;
-            node = pivot;
-
-            int balL = node->left->balance = (pb>=0) ? lb-pb-1 : lb-1;
-
-            if(lb>=0 && pb>=0)
-            {
-                int balR = node->right->balance = nb+2;
-                if(balL<=0 && balR<=0) node->balance = lb-1;
-                else if(balL<=0 && balR>0) node->balance = nb+lb+1;
-                else if(balL>0 && balR<=0) node->balance = pb;
-                else node->balance = nb+pb+2;
-            }
-            else if(lb<0 && pb>=0)
-            {
-                int balR = node->right->balance = nb-lb+2;
-                if(balL<=0 && balR<=0) node->balance = lb-1;
-                else if (balL<=0 && balR>0) node->balance = nb+1;
-                else if(balL>0 && balR<=0) node->balance = pb;
-                else node->balance = nb-lb+pb+2;
-            }
-            else if(lb>=0 && pb<0)
-            {
-                int balR = node->right->balance = nb-pb+2;
-                if(balL<=0 && balR<=0) node->balance = lb+pb-1;
-                else if(balL<=0 && balR>0) node->balance = nb+lb+1;
-                else if(balL>0 && balR<=0) node->balance = pb;
-                else node->balance = nb+2;
-            }
-            else // lb<0 && pb<0
-            {
-                int balR = node->right->balance = nb-lb-pb+2;
-                if(balL<=0 && balR<=0) node->balance = lb+pb-1;
-                else if (balL<=0 && balR>0) node->balance = nb+1;
-                else if(balL>0 && balR<=0) node->balance = pb;
-                else node->balance = nb-lb+2;
-            }
-
-        }
-
-        if(node->balance<-1) rightRotation(node->right);
-        else if(node->balance>1) leftRotation(node->right);
+        if(node->balance<-1) leftRightSwap(node);
+        else if(node->balance>1) rightLeftSwap(node);
     }
 
     void leftRotation(AVLNode<T>* &node)
     {
-        cout << "leftRot: " << node->key << endl;
-        AVLNode<T>* right = node->right;
-
         if(node->right->right)
         {
+            AVLNode<T>* right = node->right;
+
             int nb = node->balance, rb = right->balance;
 
             node->right = right->left;
@@ -148,59 +199,11 @@ private:
             int bal = node->left->balance = (rb>=0) ? nb-rb-1 : nb-1;
             node->balance = (bal>=0) ? rb-1 : rb-bal-1;
         }
-        else
-        {
-            AVLNode<T> *pivot = right->left;
-            AVLNode<T> *Q = pivot->left, *R = pivot->right;
+        else if(node->right->left) rightLeftSwap(node);
 
-            int nb = node->balance, rb = right->balance, pb = pivot->balance;
-
-            pivot->right = right;
-            pivot->left = node;
-            right->left = R;
-            node->right = Q;
-            node = pivot;
-
-            int balR = node->right->balance = (pb<=0) ? rb-pb+1 : rb+1;
-
-            if(rb<=0 && pb<=0)
-            {
-                int balL = node->left->balance = nb-2;
-                if(balL>=0 && balR>=0) node->balance = rb+1;
-                else if(balL<0 && balR>=0) node->balance = nb+rb-1;
-                else if(balL>=0 && balR<0) node->balance = pb;
-                else node->balance = nb+pb-2;
-            }
-            else if(rb>0 && pb<=0)
-            {
-                int balL = node->left->balance = nb+rb-2;
-                if(balL>=0 && balR>=0) node->balance = rb+1;
-                else if(balL<0 && balR>=0) node->balance = nb-1;
-                else if(balL>=0 && balR<0) node->balance = pb;
-                else node->balance = nb-rb+pb-2;
-            }
-            else if(rb<=0 && pb>0)
-            {
-                int balL = node->left->balance = nb-pb-2;
-                if(balL>=0 && balR>=0) node->balance = rb+pb+1;
-                else if(balL<0 && balR>=0) node->balance = nb+rb-1;
-                else if(balL>=0 && balR<0) node->balance = pb;
-                else node->balance = nb-2;
-            }
-            else // rb>0 && pb>0
-            {
-                int balL = node->left->balance = nb-rb-pb-2;
-                if(balL>=0 && balR>=0) node->balance = rb+pb+1;
-                else if (balL<0 && balR>=0) node->balance = nb-1;
-                else if(balL>=0 && balR<0) node->balance = pb;
-                else node->balance = nb-rb-2;
-            }
-        }
-
-        if(node->balance<-1) rightRotation(node->right);
-        else if(node->balance>1) leftRotation(node->right);
+        if(node->balance<-1) leftRightSwap(node);
+        else if(node->balance>1) rightLeftSwap(node);
     }
-
 
     int insert(AVLNode<T>* &node,const T& key)
     {
@@ -218,12 +221,18 @@ private:
             if(node->balance<-1) rightRotation(node);
         }
 
+        // change balance only if max height changes.
         return (abs(oldBalance)<abs(node->balance)) ? 1 : 0;
     }
 
+    /* Modified to print node->key values whenever node->balance <= -2 or
+     * node->balance >= 2.
+     */
     void snapShot(std::ostream& out,AVLNode<T> *p)
     {
-        std::cout << p->key << " " << p->balance << std::endl;
+        if(abs(p->balance)>1) std::cout << "out-of-balance" << p->key
+                                        << " " << p->balance << std::endl;
+
         out << '\"' << p->key << '\"';
         if(p->left != 0 || p->right != 0)
         {
@@ -239,6 +248,44 @@ private:
                 snapShot(out,p->right);
             out << ']';
         }
+    }
+
+    /* The below functions are for verification purposes.
+     */
+    int height(const AVLNode<T> *node) const
+    {
+        if(!node) return 0;
+        return std::max(height(node->left),height(node->right))+1;
+    }
+
+    bool isBalanced(const AVLNode<T> *parent) const
+    {
+        int trueBal  = (parent->right ? height(parent->right) : 0)
+                   - (parent->left ? height(parent->left) : 0);
+        int diff = trueBal - parent->balance;
+
+        if(diff) // print the error
+            std::cout << parent->key << " " << trueBal << " " << parent->balance << std::endl;
+        if(parent->left)
+            if(diff || !isBalanced(parent->left)) return false;
+        if(parent->right)
+            if(diff || !isBalanced(parent->right)) return false;
+        return true;
+    }
+
+    bool isValid(const AVLNode<T> *parent) const
+    {
+        if(parent->right)
+        {
+            if(parent->key <= parent->right->key) return isValid(parent->right);
+            else return false;
+        }
+        if(parent->left)
+        {
+            if(parent->key >= parent->left->key) return isValid(parent->left);
+            else return false;
+        }
+        return true;
     }
 
 };
--- Assignment5/main.cpp
+++ Assignment5/main.cpp
@@ -1,29 +1,45 @@
 #include "avl.h"
 #include <fstream>
+#include <iostream>
 #include <string>
+#include <time.h>
 
 int main()
 {
     std::string path = "C:/Programs/CMSC270/Assignment5/";
+
     AVLTree<int> x;
+    std::ofstream out;
+    bool invalid, unbalanced;
+    int max=1000000,min=-1000000,num=500000;
 
-    x.insert(0);
-    x.insert(3);
-    x.insert(2);
-    x.insert(4);
-    x.insert(5);
-    x.insert(1);
-    x.insert(-1);
-    x.insert(11);
-    x.insert(24);
-    x.insert(12);
-    x.insert(-2);
-    x.insert(-5);
+    /* Generate the tree from random integers.
+     */
+    std::cout << "Start." << std::endl;
 
-    std::ofstream out(path+"tree.txt");
+    srand((unsigned int)time(NULL));
+    for(int i=0;i!=num;i++)
+    {
+        int val = std::rand() % (1+max-min) + min;
+        x.insert(val);
+    }
+
+    /* Save the tree to disk.
+     */
+    out.open(path+"tree.txt");
     x.snapShot(out);
-
     out.close();
+
+    /* Validate the tree.
+     */
+    std::cout << "Begin verification." << std::endl;
+
+    if(invalid = !x.isValid()) std::cout << "Tree constraint not satisfied." << std::endl;
+    if(unbalanced = !x.isBalanced()) std::cout << "Tree is unbalanced." << std::endl;
+
+    std::cout << "Done." << std::endl;
+    if(!invalid && !unbalanced) std::cout << "Successfully created balanced tree of " << num
+                                    << " nodes on [" << min << "," << max << "]." << std::endl;
 
     return 0;
 }
